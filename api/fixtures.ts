0142b49e340a424a4ed3097d2fd45ad101e9b462
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2023-10-27 14:10:54 +0600

    initialized backend

---
+++ api/fixtures.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+import config from "./config";
+
+const run = async () => {
+    await mongoose.connect(config.db);
+    const db = mongoose.connection;
+    await db.close();
+}
+
+run().catch(console.error);
