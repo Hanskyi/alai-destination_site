550a883814b2f432e4c310a4a083eab39faea652
Nurdin Osmonov <oooooxg@gmail.com>
2023-11-03 12:18:02 +0600

    AD-3 created hero-section

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Reviews from "@/components/Reviews/Reviews";
 import Purpose from "@/components/Purposes/Purpose";
 import Blogs from "@/components/Blogs/Blogs";
+import HeroSection from "@/components/HeroSection/HeroSection";
 
 const Home = () => {
     return (
       <div>
+        <HeroSection />
         <Purpose />
         <Blogs />
         <Reviews />
