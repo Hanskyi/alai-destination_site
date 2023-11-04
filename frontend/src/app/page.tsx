6faf963f96e55838020f6171ab084ab96944d29b
hanskyi <han.eginbaev@gmail.com>
2023-11-04 13:11:25 +0600

    AD-4_creating block with a slider

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Reviews from "@/components/Reviews/Reviews";
+import TravelSliderBlog from "@/components/travelSliderBlog/TravelSliderBlog";
 
 const Home = () => {
     return (
       <div>
         <Reviews />
+        <TravelSliderBlog />
       </div>
     );
 };
