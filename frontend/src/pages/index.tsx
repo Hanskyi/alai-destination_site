0938ddc75eece1c8eb0b21dd18a14f57c3a6d48d
Nurdin Osmonov <oooooxg@gmail.com>
2023-11-13 20:40:42 +0600

    AD-13 adjusted Tour page, created [id].tsx for Tour

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -4,18 +4,21 @@ import TravelSliderBlog from '@/features/Home/TravelSliderBlog/TravelSliderBlog'
 import Purpose from '@/features/Home/Purpose/Purpose';
 import Blogs from '@/features/Home/Blogs/Blogs';
 import Reviews from '@/features/Home/Reviews/Reviews';
-import Header from "@/UI/Header/Header";
+import Header from '@/UI/Header/Header';
+import TourPage from '@/pages/tour-detailed';
+import Tour from '@/pages/tours/[id]';
 
 export default function Home() {
-    return (
-        <>
-          <Header />
-            <HeroSection/>
-            <TravelSliderBlog/>
-            <Purpose/>
-            <Blogs/>
-            <Reviews/>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      {/*<HeroSection />*/}
+      {/*<TravelSliderBlog />*/}
+      {/*<Purpose />*/}
+      {/*<Blogs />*/}
+      {/*<Reviews />*/}
+      <Tour />
+      <Footer />
+    </>
+  );
 }
--- frontend/src/pages/tour-detailed.tsx
+++ frontend/src/pages/tour-detailed.tsx
@@ -1,43 +1,28 @@
 import React from 'react';
-import DetailedTabs from "../features/TourPage/DetailedTabs/DetailedTabs";
-import style from "../features/TourPage/TourPage.module.scss";
+import DetailedTabs from '../features/TourPage/DetailedTabs/DetailedTabs';
+import style from '../features/TourPage/TourPage.module.scss';
+import Gallery from '@/features/TourPage/GalleryBlock/Gallery';
+import DescriptionBlock from '@/features/TourPage/DescriptionBlock/DescriptionBlock';
+import TourDetails from '@/features/TourPage/TourDetails/TourDetails';
+import TourHeroSection from '@/features/TourPage/TourHeroSection/TourHeroSection';
 
 const TourPage = () => {
-    return (
-        <div className={style.tour_page}>
-            <div className={style.banner}>
-                Banner
-            </div>
+  return (
+    <div className={style.tour_page}>
+      <TourHeroSection />
 
-            <div className="container">
-
-                <div className={style.tour_container}>
-                    <div style={{marginBottom: '50px'}}>
-                        <h4>Description</h4>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet animi debitis eaque in quasi
-                            suscipit ut! Debitis facere, ipsa magni nemo obcaecati praesentium quia repellendus soluta
-                            ut
-                            voluptatibus. Aut delectus eius eos expedita hic ipsam iusto labore, mollitia natus quam
-                            quidem
-                            quis, quisquam repudiandae sed similique sint ullam. Labore, odit.</p>
-                    </div>
-
-                    <DetailedTabs/>
-
-                    <div>
-                        <h4>Best of Alay Mountains Trek Photo Gallery</h4>
-                        <div className={style.gallery_placeholder}>
-                            Galley
-                        </div>
-                    </div>
-                </div>
-
-
-            </div>
-
-
+      <div className="container">
+        <div className={style.parentContainer}>
+          <div className={style.tour_container}>
+            <DescriptionBlock />
+            <DetailedTabs />
+            <Gallery />
+          </div>
+          <TourDetails />
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default TourPage;
