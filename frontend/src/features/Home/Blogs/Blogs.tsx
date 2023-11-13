14c71584c2ec2a24bc854e6a880c78ac717960e7
RasidaSydykova <sydykova.rasida@mail.ru>
2023-11-13 12:59:52 +0600

    AD-5 refactored code Blogs block

--- frontend/src/features/Home/Blogs/Blogs.tsx
+++ frontend/src/features/Home/Blogs/Blogs.tsx
@@ -5,15 +5,13 @@ import style from './Blogs.module.scss';
 const Blogs = () => {
   return (
     <div className={`${style.blogs} container`}>
-      <div className={style.blogs}>
-        <h3 className={style.blogs_title}>How your travel makes a difference</h3>
+      <h3 className={style.blogs_title}>How your travel makes a difference</h3>
 
-        <div className={style.blogs_cards}>
-          <BlogItem />
-          <BlogItem />
-          <BlogItem />
-          <BlogItem />
-        </div>
+      <div className={style.blogs_cards}>
+        <BlogItem />
+        <BlogItem />
+        <BlogItem />
+        <BlogItem />
       </div>
     </div>
   );
