39072299cb5eeed860f72d3d070d39cae6096a0b
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-11-17 02:40:24 +0600

    AD-23 Laid out the main and middle blocks.  Added component and dependency for animation.  Loaded the required icons.

---
+++ frontend/src/pages/about-us.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import AboutUsPage from "@/features/AboutUs/AboutUsPage";
+import Header from "@/UI/Header/Header";
+import Footer from "@/UI/Footer/Footer";
+
+const AboutUs = () => {
+  return (
+    <>
+      <Header/>
+      <AboutUsPage/>
+      <Footer/>
+    </>
+  );
+};
+
+export default AboutUs;
