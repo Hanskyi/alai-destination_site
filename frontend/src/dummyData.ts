227a6ba99a788d7c4375dddbd0a91fff33a234d4
Dmitrii Morrows <trollman1993@gmail.com>
2023-11-17 17:06:37 +0600

    AD-21 filter by rating component has been created

---
+++ frontend/src/dummyData.ts
@@ -0,0 +1,12 @@
+interface IReviewsCount {
+  rating: number;
+  count: number;
+}
+
+export const REVIEWS_COUNT: IReviewsCount[] = [
+  { rating: 5, count: 715 },
+  { rating: 4, count: 38 },
+  { rating: 3, count: 6 },
+  { rating: 2, count: 4 },
+  { rating: 1, count: 2 },
+];
