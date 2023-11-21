5fb7ded8dc54e56f92d0e7fc04e0060b3fd9fd5b
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-11-21 08:05:39 +0600

    AD-23 Fixed errors after installing Husky, removed logoSmall, renamed Header to Toolbar.

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -2,14 +2,14 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
 import { wrapper } from '@/store/store';
 import { Provider } from 'react-redux';
-import Header from '@/UI/Header/Header';
+import Toolbar from '@/UI/Toolbar/Toolbar';
 import Footer from '@/UI/Footer/Footer';
 
 export default function App({ Component, ...rest }: AppProps) {
   const { store, props } = wrapper.useWrappedStore(rest);
   return (
     <Provider store={store}>
-      <Header />
+      <Toolbar />
        <Component {...props.pageProps} />
       <Footer />
     </Provider>
