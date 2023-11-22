6e684d851837be84023ff5a360d20c99977f08cb
Iskender Shamaldaev <sh.iskander012@gmail.com>
2023-11-23 02:15:44 +0600

    AD-23 Fixed errors, done Our Partners block, added photos.

--- frontend/src/features/AboutUs/AboutUsPage.tsx
+++ frontend/src/features/AboutUs/AboutUsPage.tsx
@@ -2,7 +2,13 @@ import React from 'react';
 import aboutUsStyle from '@/features/AboutUs/AboutUs.module.scss';
 import ValuesBlock from '@/features/AboutUs/Components/ValuesBlock';
 import CountersBlock from '@/features/AboutUs/Components/CountersBlock';
+import Image from "next/image";
 
+import Kato from '../../assets/icon/about-us/kato.png';
+import Alay from '../../assets/icon/about-us/visit_alay.png';
+import Osh from '../../assets/icon/about-us/osh.png';
+import Karakol from '../../assets/icon/about-us/karakol.png';
+import Jyrgalan from '../../assets/icon/about-us/jyrgalan.png';
 const AboutUsPage = () => {
   return (
     <>
@@ -40,6 +46,64 @@ const AboutUsPage = () => {
             <CountersBlock />
           </div>
         </div>
+
+        <div className="container">
+          <div className={aboutUsStyle.partnersBlock}>
+            <h1 className={aboutUsStyle.partnersBlock__title}>Our Trusted Partners</h1>
+            <p className={aboutUsStyle.partnersBlock__text}>Lorem ipsum dolor sit amet
+              consectetur adipiscing elit Etiam at ipsum at ligula vestibulum
+              sodales Sed luctus orci vel nibh aliquam laoreet Aenean accumsan
+              sodales Sed luctus orci vel nibh aliquam laoreet Aenean accumsan</p>
+
+            <div className={aboutUsStyle.partnersBlock__cards}>
+                <a className={aboutUsStyle.partnersBlock__partner}
+                   href="https://wtk.kato.kg/ru"
+                   target="_blank"
+                >
+                  <Image
+                    className={aboutUsStyle.partnersBlock__img}
+                    width={140} height={70} src={ Kato } alt="kato" />
+                </a>
+
+                <a className={aboutUsStyle.partnersBlock__partner}
+                   href="https://visitalay.com/"
+                   target="_blank"
+                >
+                  <Image
+                    className={aboutUsStyle.partnersBlock__img}
+                    width={140} height={70} src={ Alay } alt="visit_alay" />
+                </a>
+
+                <a className={aboutUsStyle.partnersBlock__partner}
+                   href="https://destinationosh.com/ru/"
+                   target="_blank"
+                >
+                  <Image
+                    className={aboutUsStyle.partnersBlock__img}
+                    width={140} height={70} src={ Osh } alt="osh" />
+                </a>
+
+                <a className={aboutUsStyle.partnersBlock__partner}
+                   href="https://destinationkarakol.com/ru/o-nas/o-destinacii-karakol/"
+                   target="_blank"
+                >
+                  <Image
+                    className={aboutUsStyle.partnersBlock__img}
+                    width={140} height={70} src={ Karakol } alt="karakol" />
+                </a>
+
+                <a className={aboutUsStyle.partnersBlock__partner}
+                   href="https://jyrgalan.com/ru/"
+                   target="_blank"
+                >
+                  <Image
+                    className={aboutUsStyle.partnersBlock__img}
+                    width={140} height={70} src={ Jyrgalan } alt="jyrgalan" />
+                </a>
+            </div>
+          </div>
+        </div>
+
       </div>
     </>
   );
