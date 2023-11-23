8d62f90fcd3642f09a37c90196a3004616f62a17
RasidaSydykova <sydykova.rasida@mail.ru>
2023-11-24 02:23:21 +0600

    AD-25 created page for one blog page, edited some files

--- frontend/src/features/Home/Blogs/BlogItem.tsx
+++ frontend/src/features/Home/Blogs/BlogItem.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import style from './Blogs.module.scss';
+import Link from 'next/link';
+
 const BlogItem = () => {
   return (
     <div className={style.blog}>
-      <span className={style.blog_date}>21 Mar 2023</span>
-      <p className={style.blog_text}>
-        Walking the talk: 22 highlights from Intrepid’s 2022 Impact Report
-      </p>
-      <span className={style.blog_subtext}>The Good Times</span>
+      <Link href="/articles/1">
+        <span className={style.blog_date}>21 Mar 2023</span>
+        <p className={style.blog_text}>
+          Walking the talk: 22 highlights from Intrepid’s 2022 Impact Report
+        </p>
+        <span className={style.blog_subtext}>The Good Times</span>
+      </Link>
     </div>
   );
 };
