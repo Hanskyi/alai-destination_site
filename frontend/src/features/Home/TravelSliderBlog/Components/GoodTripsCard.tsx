932c8306d74b81d509a59cef770b7b2478a6c859
hanskyi <han.eginbaev@gmail.com>
2023-11-24 01:23:48 +0600

    AD-33 refactor slider

--- frontend/src/features/Home/TravelSliderBlog/Components/GoodTripsCard.tsx
+++ frontend/src/features/Home/TravelSliderBlog/Components/GoodTripsCard.tsx
@@ -2,26 +2,31 @@ import React from 'react';
 import style from '../TravelSliderBlog.module.scss';
 import Image from 'next/image';
 
-const GoodTripsCard = () => {
+interface Props {
+    item?: string;
+}
+const GoodTripsCard: React.FC<Props>= ({item}) => {
   return (
-    <div className={style.card}>
-      <div className={style.card__image__box}>
-        <Image
-          width={1200}
-          height={1200}
-          className={style.card__image__box__content__two}
-          src="https://core-renderer-tiles.maps.yandex.net/tiles?l=map&v=23.11.01-1-b231030182430&x=6&y=3&z=3&scale=2.625&lang=ru_RU&client_id=yandex-web-maps&&ads=enabled"
-          alt="#"
-        />
-      </div>
-      <h4 className={style.card__days}>11 Days · Comfort</h4>
-      <p className={style.card__description}>
-        Best of Antarctica: A White Christmas (Ocean Endeavour)
-      </p>
-      <p className={style.card__price}>
-        From <span className={style.card__price__span}>USD $9,990</span>
-      </p>
-    </div>
+        <div className={style.card}>
+            <div className={style.card__image__box}>
+                <Image
+                    width={1200}
+                    height={1200}
+                    className={style.card__image__box__content__two}
+                    src="https://core-renderer-tiles.maps.yandex.net/tiles?l=map&v=23.11.01-1-b231030182430&x=6&y=3&z=3&scale=2.625&lang=ru_RU&client_id=yandex-web-maps&&ads=enabled"
+                    alt="#"
+                />
+            </div>
+           <div className={style.card__body}>
+               <h4 className={style.card__days}>11 Days · Comfort</h4>
+               <p className={style.card__description}>
+                   Best of Antarctica: A White Christmas (Ocean Endeavour)
+               </p>
+               <p className={style.card__price}>
+                   From <span className={style.card__price__span}>USD $9,990</span>
+               </p>
+           </div>
+        </div>
   );
 };
 
--- frontend/src/features/Home/TravelSliderBlog/TravelSliderBlog.tsx
+++ frontend/src/features/Home/TravelSliderBlog/TravelSliderBlog.tsx
@@ -1,27 +1,35 @@
 import React from 'react';
 import style from './TravelSliderBlog.module.scss';
-import SwiperBundleWaysToTravel from '@/features/Home/TravelSliderBlog/Components/SwiperBundleWaysToTravel';
-import SwiperBundleGoodTrips from '@/features/Home/TravelSliderBlog/Components/SwiperBundleGoodTrips';
+import GoodTripsCard from "@/features/Home/TravelSliderBlog/Components/GoodTripsCard";
+import SwiperComponent from "@/components/SwiperComponent/SwiperComponent";
+import WaysToTravelCard from "@/features/Home/TravelSliderBlog/Components/WaysToTravelCard";
+
 
 const TravelSliderBlog = () => {
-  return (
-    <div className={`${style.travelSliderBlog} container`}>
-      <h3 className={style.travelSliderBlog__title}>Small group travel that good all over.</h3>
-      <p className={style.travelSliderBlog__text}>
-        Good views, good friends and good times on over 1000 trips in more than 100 countries
-      </p>
+    const content = ['qwd', 'dwq', 'qwd', 'dwq', 'qwd', 'dwq', 'qwd', 'dwq', 'qwd', 'dwq'];
 
-      <div className={style.travelSliderBlog__first}>
-        <p className={style.travelSliderBlog__description}> Ways to travel</p>
-        <SwiperBundleWaysToTravel />
-      </div>
+    return (
+        <div className={`${style.travelSliderBlog} container`}>
+            <h3 className={style.travelSliderBlog__title}>Small group travel that good all over.</h3>
+            <p className={style.travelSliderBlog__text}>
+                Good views, good friends and good times on over 1000 trips in more than 100 countries
+            </p>
 
-      <div className="travelSliderBlog__second">
-        <p className={style.travelSliderBlog__description}> Good Trips Only for 2023</p>
-        <SwiperBundleGoodTrips />
-      </div>
-    </div>
-  );
+            <div className={style.travelSliderBlog__first}>
+                <p className={style.travelSliderBlog__description}> Ways to travel</p>
+                <SwiperComponent  content={content}>
+                    <WaysToTravelCard/>
+                </SwiperComponent>
+            </div>
+
+            <div className="travelSliderBlog__second">
+                <p className={style.travelSliderBlog__description}> Good Trips Only for 2023</p>
+                <SwiperComponent content={content} >
+                    <GoodTripsCard/>
+                </SwiperComponent>
+            </div>
+        </div>
+    );
 };
 
 export default TravelSliderBlog;
