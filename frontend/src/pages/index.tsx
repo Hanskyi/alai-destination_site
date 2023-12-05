3ff08f1ff9a644979967177a1b177d1c5e8f8006
Dmitrii Morrows <trollman1993@gmail.com>
2023-12-05 21:47:46 +0600

    AD-63 comment lines removed

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -10,19 +10,6 @@ const Home = () => {
   );
 };
 
-// Home.getInitialProps = wrapper.getInitialPageProps((store) => async (context) => {
-//   await store.dispatch(fetchHomeData(context.locale ? context.locale : 'en'));
-//   return {
-//     name: 'Home Page',
-//     props: {
-//       // You can get the messages from anywhere you like. The recommended
-//       // pattern is to put them in JSON files separated by locale and read
-//       // the desired one based on the `locale` received from Next.js.
-//       messages: (await import(`../../lang/${context.locale}.json`)).default,
-//     },
-//   };
-// });
-
 export const getStaticProps = wrapper.getStaticProps((store) => async (context) => {
   await store.dispatch(fetchHomeData(context.locale ? context.locale : 'en'));
   return {
