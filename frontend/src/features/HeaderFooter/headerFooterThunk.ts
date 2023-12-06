f1d3e8ef4990bfb57305ddff362c09ccd6e9fefe
arslanbekbolotov <arslanbekbolotov58@gmail.com>
2023-12-06 19:35:09 +0600

    reformatted code

--- frontend/src/features/HeaderFooter/headerFooterThunk.ts
+++
@@ -1,9 +0,0 @@
-import { createAsyncThunk } from '@reduxjs/toolkit';
-import { IHeaderFooterInfo } from '@/type';
-import axiosApi from '@/axiosApi';
-
-export const fetchHFData = createAsyncThunk<IHeaderFooterInfo>('fetchHF', async () => {
-  const { data } = await axiosApi<IHeaderFooterInfo>(`info`);
-
-  return data;
-});
