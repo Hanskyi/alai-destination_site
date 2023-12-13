6ff79d9cfaf950d41129d65af1ff2d5dea311741
Nurdin Osmonov <oooooxg@gmail.com>
2023-12-13 18:06:38 +0600

    AD-22 filtering tours on the backend by location, classification, duration. Generating dynamic query.

--- frontend/src/pages/tours.tsx
+++ frontend/src/pages/tours.tsx
@@ -23,18 +23,19 @@ import {
 } from '@/type';
 import { GetServerSideProps } from 'next';
 import axiosApi from '@/axiosApi';
-//
-// interface Props {
-//   tours: Tour[];
-//   locations: ILocalizationShortInfo[];
-//   classifications: ILocalizationShortInfoClassification[];
-// }
 
-const Tours = () => {
+interface Props {
+  tours: Tour[];
+  locations: ILocalizationShortInfo[];
+  classifications: ILocalizationShortInfoClassification[];
+}
+
+const Tours = ({ tours, locations, classifications }: Props) => {
   const router = useRouter();
-  const tours = useAppSelector(selectTours);
-  const locations = useAppSelector(selectLocations);
-  const classifications = useAppSelector(selectClassifications);
+
+  // const tours = useAppSelector(selectTours);
+  // const locations = useAppSelector(selectLocations);
+  // const classifications = useAppSelector(selectClassifications);
 
   return (
     <FilteredTours
@@ -45,94 +46,97 @@ const Tours = () => {
     />
   );
 };
-//
-// export const getServerSideProps: GetServerSideProps<Props> = wrapper.getServerSideProps(
-//   (store) => async (context) => {
-//     try {
-//
-//       const queryParameters = [];
-//
-//       queryParameters.push('fields[0]=id');
-//       queryParameters.push('fields[1]=title');
-//       queryParameters.push('fields[2]=price');
-//       queryParameters.push('fields[3]=duration');
-//
-//       if (context.query.classification) {
-//         queryParameters.push(
-//           `filters[classification][title][$eq]=${encodeURIComponent(context.query.classification)}`,
-//         );
-//       }
-//
-//       if (context.query.location) {
-//         queryParameters.push(
-//           `filters[location][name][$eq]=${encodeURIComponent(context.query.location)}`,
-//         );
-//       }
-//
-//       queryParameters.push('populate[classification][fields][0]=title');
-//       queryParameters.push('populate[location][fields][0]=name');
-//       queryParameters.push('populate[mainImage][fields][0]=url');
-//
-//       const queryString = queryParameters.join('&');
-//
-//       let toursUrl = `tours?fields[0]=id&fields[1]=title&fields[2]=price&fields[3]=duration&populate[classification][fields][0]=title&populate[location][fields][0]=name&populate[mainImage][fields][0]=url&populate[localizations][populate]=true&populate[localizations][fields][0]=locale`;
-//
-//       if (context.query.location || context.query.classification) {
-//         toursUrl = `tours?${queryString}`;
-//       }
-//       const toursResponse = await axiosApi.get<ToursPage>(`${toursUrl}&locale=${context.locale}`);
-//       const tours = toursResponse.data.data;
-//
-//
-//
-//       const locationsUrl =
-//         'locations?fields[0]=name&fields[1]=locale&populate[localizations][populate]=true&populate[localizations][fields][0]=locale';
-//       const locationsResponse = await axiosApi.get<ILocationListShortInfo>(
-//         `${locationsUrl}&locale=${context.locale}`,
-//       );
-//       const locations = locationsResponse.data.data;
-//
-//       const classificationsUrl =
-//         'classifications?fields[0]=title&fields[1]=locale&populate[localizations][populate]=true&populate[localizations][fields][0]=locale';
-//       const classificationsResponse = await axiosApi.get<IClassificationListShortInto>(
-//         `${classificationsUrl}&locale=${context.locale}`,
-//       );
-//       const classifications = classificationsResponse.data.data;
-//
-//       return {
-//         props: {
-//           tours,
-//           locations,
-//           classifications,
-//           messages: (await import(`../../lang/${context.locale}.json`)).default,
-//         },
-//       };
-//     } catch (error) {
-//       console.error('Error in getServerSideProps:', error);
-//       return {
-//         props: {
-//           tours: { data: [] },
-//           locations: { data: [] },
-//           classifications: { data: [] },
-//           messages: (await import(`../../lang/${context.locale}.json`)).default,
-//         },
-//       };
-//     }
-//   },
-// );
 
-export const getStaticProps = wrapper.getStaticProps((store) => async (context) => {
-  const tours = store.dispatch(fetchToursData(context.locale ? context.locale : 'en'));
-  const locations = store.dispatch(fetchLocations(context.locale ? context.locale : 'en'));
-  const classifications = store.dispatch(
-    fetchClassifications(context.locale ? context.locale : 'en'),
-  );
-  await Promise.all([tours, locations, classifications]);
-  return {
-    props: {
-      messages: (await import(`../../lang/${context.locale}.json`)).default,
-    },
-  };
-});
+export const getServerSideProps: GetServerSideProps<Props> = wrapper.getServerSideProps(
+  (store) => async (context) => {
+    try {
+      const queryParameters = [];
+
+      queryParameters.push('fields[0]=id');
+      queryParameters.push('fields[1]=title');
+      queryParameters.push('fields[2]=price');
+      queryParameters.push('fields[3]=duration');
+
+      if (context.query.classification) {
+        queryParameters.push(
+          `filters[classification][title][$eq]=${encodeURIComponent(
+            context.query.classification as string,
+          )}`,
+        );
+      }
+
+      if (context.query.location) {
+        queryParameters.push(
+          `filters[location][name][$eq]=${encodeURIComponent(context.query.location as string)}`,
+        );
+      }
+
+      if (context.query.duration) {
+        queryParameters.push(`filters[duration][$gte]=${context.query.duration as string}`);
+      }
+
+      queryParameters.push('populate[classification][fields][0]=title');
+      queryParameters.push('populate[location][fields][0]=name');
+      queryParameters.push('populate[mainImage][fields][0]=url');
+
+      const queryString = queryParameters.join('&');
+
+      let toursUrl = `tours?fields[0]=id&fields[1]=title&fields[2]=price&fields[3]=duration&populate[classification][fields][0]=title&populate[location][fields][0]=name&populate[mainImage][fields][0]=url&populate[localizations][populate]=true&populate[localizations][fields][0]=locale`;
+
+      if (context.query.location || context.query.classification || context.query.duration) {
+        toursUrl = `tours?${queryString}`;
+      }
+      const toursResponse = await axiosApi.get<ToursPage>(`${toursUrl}&locale=${context.locale}`);
+      const tours = toursResponse.data.data;
+
+      const locationsUrl =
+        'locations?fields[0]=name&fields[1]=locale&populate[localizations][populate]=true&populate[localizations][fields][0]=locale';
+      const locationsResponse = await axiosApi.get<ILocationListShortInfo>(
+        `${locationsUrl}&locale=${context.locale}`,
+      );
+      const locations = locationsResponse.data.data;
+
+      const classificationsUrl =
+        'classifications?fields[0]=title&fields[1]=locale&populate[localizations][populate]=true&populate[localizations][fields][0]=locale';
+      const classificationsResponse = await axiosApi.get<IClassificationListShortInto>(
+        `${classificationsUrl}&locale=${context.locale}`,
+      );
+      const classifications = classificationsResponse.data.data;
+
+      return {
+        props: {
+          tours,
+          locations,
+          classifications,
+          messages: (await import(`../../lang/${context.locale}.json`)).default,
+        },
+      };
+    } catch (error) {
+      console.error('Error in getServerSideProps:', error);
+      return {
+        props: {
+          tours: [],
+          locations: [],
+          classifications: [],
+          messages: (await import(`../../lang/${context.locale}.json`)).default,
+        },
+      };
+    }
+  },
+);
+
+// export const getStaticProps = wrapper.getStaticProps((store) => async (context) => {
+//   const tours = store.dispatch(fetchToursData(context.locale ? context.locale : 'en'));
+//   const locations = store.dispatch(fetchLocations(context.locale ? context.locale : 'en'));
+//   const classifications = store.dispatch(
+//     fetchClassifications(context.locale ? context.locale : 'en'),
+//   );
+//   await Promise.all([tours, locations, classifications]);
+//   return {
+//     props: {
+//       messages: (await import(`../../lang/${context.locale}.json`)).default,
+//     },
+//   };
+// });
 
 export default Tours;
